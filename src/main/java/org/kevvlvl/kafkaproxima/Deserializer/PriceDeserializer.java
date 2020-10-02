4f714b25e0e4bc5c1aa057dc8ca128959b87af18
Kevork <ksayad@gmail.com>
2020-10-02 19:24:32 -0400

    Use Jackson library

--- src/main/java/org/kevvlvl/kafkaproxima/Deserializer/PriceDeserializer.java
+++ src/main/java/org/kevvlvl/kafkaproxima/Deserializer/PriceDeserializer.java
@@ -1,9 +1,9 @@
 package org.kevvlvl.kafkaproxima.Deserializer;
 
-import io.quarkus.kafka.client.serialization.JsonbDeserializer;
+import io.quarkus.kafka.client.serialization.ObjectMapperDeserializer;
 import org.kevvlvl.kafkaproxima.model.Price;
 
-public class PriceDeserializer extends JsonbDeserializer<Price> {
+public class PriceDeserializer extends ObjectMapperDeserializer<Price> {
 
     public PriceDeserializer() {
         super(Price.class);
